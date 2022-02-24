f7c5259440a28773b1453228752b18288eba2400
sayan das <sayanwastaken@gmail.com>
2022-02-24 11:18:17 +0530

    some changes

--- backend/src/models/restaurants.model.js
+++ backend/src/models/restaurants.model.js
@@ -5,7 +5,7 @@ const restaurantSchema = new mongoose.Schema(
     name: { type: String, required: true },
     about: { type: String, required: true },
     price: { type: String, required: true },
-    type: { type: String, required: true },
+    type: [{ type: String, required: true }],
     bestItems: [{ type: String, required: true }],
     features: [{ type: String, required: true }],
   },
